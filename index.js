333857659e10c5b09a707f25dae5d3527244ab7d
Hasitha Liyanage <hasithaliyan@gmail.com>
2015-01-31 13:11:24 +0530

    modified template names to use / instead of .

--- index.js
+++ index.js
@@ -6,39 +6,42 @@ var gutil = require('gulp-util');
 var Emblem = require('emblem');
 var EmberHandlebars = require('ember-template-compiler').EmberHandlebars;
 
-module.exports = function(options) {
-  var opts = options || {};
-  var compilerOptions = opts.compilerOptions || {};
-  
-  return through(function(file) {
-    if (file.isNull()) { 
-      return this.queue(file); 
-    } // pass along
-    
-    if (file.isStream()) { 
-      return this.emit('error', new gutil.PluginError('gulp-ember-emblem', 'Streaming not supported')); 
-    }
-    
-    var contents = file.contents.toString();
-    var compiled = null;
-    try { 
-      compiled = Emblem.precompile(EmberHandlebars, contents, compilerOptions).toString(); 
-    } catch (err) { 
-      this.emit('error', err); 
-    }
-    
-    if (compiled) {
-      file.contents = new Buffer(compiled);
-      file.path = gutil.replaceExtension(file.path, '.js');
-      file.defineModuleOptions = {
-        require: { Ember: 'ember' },
-        context: {
-          emberHandlebars: 'Ember.Handlebars.template(<%= contents %>)'
-        },
-        wrapper: '(Ember.TEMPLATES[\'<%= name %>\'] = <%= emberHandlebars %>)'
-      };
-      this.queue(file);
-    }
-    
-  });
+module.exports = function (options) {
+    var opts = options || {};
+    var compilerOptions = opts.compilerOptions || {};
+
+    return through(function (file) {
+        if (file.isNull()) {
+            return this.queue(file);
+        } // pass along
+
+        if (file.isStream()) {
+            return this.emit('error', new gutil.PluginError('gulp-ember-emblem', 'Streaming not supported'));
+        }
+
+        var contents = file.contents.toString();
+        var compiled = null;
+        try {
+            compiled = Emblem.precompile(EmberHandlebars, contents, compilerOptions).toString();
+        } catch (err) {
+            this.emit('error', err);
+        }
+
+        if (compiled) {
+            file.contents = new Buffer(compiled);
+            file.path = gutil.replaceExtension(file.path, '.js');
+            file.defineModuleOptions = {
+                require: {
+                    Ember: 'ember'
+                },
+                context: {
+                    emberHandlebars: 'Ember.Handlebars.template(<%= contents %>)',
+                    templateName: path.basename(file.path).replace('.js', '').replace('.', '/')
+                },
+                wrapper: '(Ember.TEMPLATES[\'<%= templateName %>\'] = <%= emberHandlebars %>)'
+            };
+            this.queue(file);
+        }
+
+    });
 };
