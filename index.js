aae97b7365fbf8b75efb4f9e972fd3aeb29e7479
Hasitha Liyanage <hasithaliyan@gmail.com>
2015-01-31 12:56:19 +0530

    spacing

--- index.js
+++ index.js
@@ -9,13 +9,24 @@ var EmberHandlebars = require('ember-template-compiler').EmberHandlebars;
 module.exports = function(options) {
   var opts = options || {};
   var compilerOptions = opts.compilerOptions || {};
+  
   return through(function(file) {
-    if (file.isNull()) { return this.queue(file); } // pass along
-    if (file.isStream()) { return this.emit('error', new gutil.PluginError('gulp-ember-emblem', 'Streaming not supported')); }
+    if (file.isNull()) { 
+      return this.queue(file); 
+    } // pass along
+    
+    if (file.isStream()) { 
+      return this.emit('error', new gutil.PluginError('gulp-ember-emblem', 'Streaming not supported')); 
+    }
+    
     var contents = file.contents.toString();
     var compiled = null;
-    try { compiled = Emblem.precompile(EmberHandlebars, contents, compilerOptions).toString(); }
-    catch (err) { this.emit('error', err); }
+    try { 
+      compiled = Emblem.precompile(EmberHandlebars, contents, compilerOptions).toString(); 
+    } catch (err) { 
+      this.emit('error', err); 
+    }
+    
     if (compiled) {
       file.contents = new Buffer(compiled);
       file.path = gutil.replaceExtension(file.path, '.js');
@@ -28,5 +39,6 @@ module.exports = function(options) {
       };
       this.queue(file);
     }
+    
   });
 };
